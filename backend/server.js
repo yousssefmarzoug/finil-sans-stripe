e6e1a77893c261f5a886b180e07777a8ee8c6a41
youssef marzoug <youssefmarzoug19@gmail.com>
2024-02-16 00:10:01 +0100

    rrr

--- backend/server.js
+++ backend/server.js
@@ -104,8 +104,8 @@ routes.get("/", (req, res) => {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV == 'production') {
+
 	app.use(express.static(path.join(__dirname, '/client/build')));
 
 	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+
